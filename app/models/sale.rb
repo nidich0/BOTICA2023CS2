b68f4eb3a4101b914961e258d98bf20dacb4202e
Frank Condezo <frankcondezo@gmail.com>
2023-09-27 17:11:20 -0500

    add some missing fields

--- app/models/sale.rb
+++ app/models/sale.rb
@@ -1,5 +1,4 @@
 class Sale < ApplicationRecord
     has_many :sales_details
-    has_many :price , through: :sales_details
     belongs_to :client
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,21 +1,19 @@
 Rails.application.routes.draw do
   resources :items
   resources :clients
-  resources :products 
+  resources :products
 
-
- 
   get 'download_prueba/:id', to: 'sales#download_prueba'
   get 'download_pdf/:id', to: 'sales#download_pdf'
   get 'showSaleDetails/:id', to: 'sales#showSaleDetails'
-  resources :sales 
+  resources :sales
   # get 'dashboard_seller/index'
   get 'dashboard_seller', to: 'dashboard_seller#index'
   root 'home#index'
   devise_for :users
 
   namespace :admin do
-    resources :suppliers, :products, :categories, :clients
+    resources :suppliers, :products, :categories, :clients, :sales
     root 'suppliers#index'
   end
 
