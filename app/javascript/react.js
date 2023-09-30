f2f6fecda2bb03938d954d092cbd0d1b026ec9c9
Frank Condezo <frankcondezo@gmail.com>
2023-09-30 18:13:37 -0500

    fix render component using turbo callbacks

--- app/javascript/react.js
+++ app/javascript/react.js
@@ -1,10 +1,14 @@
 import React from "react";
-import { createRoot } from 'react-dom/client';
+import { createRoot } from "react-dom/client";
 import Seller from "./components/Seller";
 
-const container = document.getElementById("root");
-
-if (container) {
-  const root = createRoot(container);
-  root.render(<Seller />);
+function renderComponent() {
+  const container = document.getElementById("root");
+  if (container) {
+    const root = createRoot(container);
+    root.render(<Seller />);
+  }
 }
+
+document.addEventListener("DOMContentLoaded", renderComponent);
+document.addEventListener("turbo:render", renderComponent);
